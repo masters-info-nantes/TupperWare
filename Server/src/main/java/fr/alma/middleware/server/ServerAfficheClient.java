d8ccb14f392459eb1e2cf49050d5517346cc1adf
nBourdin <nicolas.bourdin@mail.com>
2015-10-01 22:34:17 +0200

    add impl serv

---
+++ Server/src/main/java/fr/alma/middleware/server/ServerAfficheClient.java
@@ -0,0 +1,15 @@
+package fr.alma.middleware.server;
+
+import java.rmi.RemoteException;
+
+import fr.alma.middleware.remote.InterfaceAffichageClient;
+
+public class ServerAfficheClient implements InterfaceAffichageClient {
+
+	@Override
+	public void affiche(String Message) throws RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
